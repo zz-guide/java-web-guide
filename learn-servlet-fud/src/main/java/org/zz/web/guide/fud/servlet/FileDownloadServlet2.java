344a91960aaf9ef0c46b9b012a43d9421381c899
xulei131401 <373045134@qq.com>
2024-07-22 01:13:43 +0800

    fix

--- learn-servlet-fud/src/main/java/org/zz/web/guide/fud/servlet/FileDownloadServlet2.java
+++ learn-servlet-fud/src/main/java/org/zz/web/guide/fud/servlet/FileDownloadServlet2.java
@@ -1,12 +1,16 @@
 package org.zz.web.guide.fud.servlet;
 
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.Serial;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @WebServlet("/file/download/t2")
 public class FileDownloadServlet2 extends HttpServlet {
@@ -16,5 +20,25 @@ public class FileDownloadServlet2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("=== 下载其他远程服务器文件 ===");
+        String remoteUrl = "https://i0.hdslb.com/bfs/archive/6e8e16d02a0f0c1cf0b246e13a5bf3eadef52d4e.jpg";
+        String[] bb = remoteUrl.split("[?]")[0].split("/");
+        String filename = bb[bb.length - 1];
+
+        URL url = new URL(remoteUrl);
+        HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+        resp.setContentType("application/octet-stream");
+        resp.setHeader("Content-Disposition", "attachment;filename="+filename);
+
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.write(bufferedInputStream.readAllBytes());
+
+        outputStream.flush();
+        outputStream.close();
+        bufferedInputStream.close();
+
+        System.out.println("文件下载成功");
     }
 }
